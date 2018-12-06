c647f5ebac7a5cb7f31bfcd148519d2a3b0604d7
Daniel <danielamram@campus.technion.ac.il>
2018-12-07 00:06:10 +0200

    Update send.py

--- exercises/p4runtime/send.py
+++ exercises/p4runtime/send.py
@@ -12,6 +12,7 @@ from scapy.all import Ether, IP, UDP, TCP
 
 host_name_to_ip_dic = {"h1": "10.0.1.1", "h2": "10.0.2.2", "h3": "10.0.3.3"}
 
+
 def get_if():
     ifs=get_if_list()
     iface=None # "h1-eth0"
@@ -20,23 +21,25 @@ def get_if():
             iface=i
             break;
     if not iface:
-        print "Cannot find eth0 interface"
+        print("Cannot find eth0 interface")
         exit(1)
     return iface
+
 
 def main():
 
     if len(sys.argv)<3:
-        print 'pass 2 arguments: <destination> "<message>"'
+        print('pass 2 arguments: <destination> "<message>"')
         exit(1)
 
-    host_name = socket.gethostbyname(sys.argv[1])
-    addr = host_name_to_ip_dic[host_name]
-
-
+    try:
+        host_ip = host_name_to_ip_dic[sys.argv[1]]
+    except KeyError:
+        print("use host name (./send.py h2 message)")
+    host_name = socket.gethostbyname(host_ip)
     iface = get_if()
 
-    print "sending on interface %s to %s" % (iface, str(addr))
+    print("sending on interface %s to %s" % (iface, str(addr)))
     pkt =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
     pkt = pkt /IP(dst=addr) / TCP(dport=1234, sport=random.randint(49152,65535)) / sys.argv[2]
     pkt.show2()
